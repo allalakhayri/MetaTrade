5629f5bb3402287bd584441273b12e6f81130872
khayri <khayri.allala2018@gmai.com>
2023-12-03 02:39:13 +0100

    adding blockChain part

---
+++ BlockChainFiles/blockchain.js
@@ -0,0 +1,64 @@
+const Block = require('./block');
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+    this.difficulty = 5;
+  }
+  
+  // create genesis block
+  createGenesisBlock () {
+    return new Block(0, Date.now(), "Genesis block", "0");
+  }
+  
+  // return latest block of chain.
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+  
+  // add a new block to chain
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.mineBlock(this.difficulty);
+    this.chain.push(newBlock);
+  }
+  
+  // verify if chain is valid
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+      
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+// module.exports = Blockchain
+
+let sampleCoin = new Blockchain();
+console.log('Mining block 1...');
+sampleCoin.addBlock(new Block(1, Date.now(), { amount: 4 }));
+
+console.log('Mining block 2...');
+sampleCoin.addBlock(new Block(2, Date.now(), { amount: 6 }));
+console.log('Blockchain valid? ' + sampleCoin.isChainValid());
+
+
+console.log('Try changing a block...');
+sampleCoin.chain[1].data = { amount: 100 };
+
+
+sampleCoin.chain[1].hash = sampleCoin.chain[1].calculateHash();
+console.log("Blockchain valid? " + sampleCoin.isChainValid());
+
+console.log(JSON.stringify(sampleCoin, null, 4));
+
+console.log('getLastBlock: ', sampleCoin.getLatestBlock());
